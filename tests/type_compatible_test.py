af9e87c7dbe3555dd89f8ea05c7794a766fd76af
raumedi <raul.medina@alumnos.uva.es>
2019-04-28 20:09:48 +0200

    added compatibility for: integer, double, interval, boolean and date (not implemented yet)

--- tests/type_compatible_test.py
+++ tests/type_compatible_test.py
@@ -9,3 +9,31 @@ def test_compatible_posgresql_strings():
     assert type_compatible.is_string(char)
     assert type_compatible.is_string(varchar)
     assert type_compatible.is_string(nchar)
+
+def test_compatible_posgresql_integer():
+    num = "integer"
+    snum = "smallint"
+
+    assert type_compatible.is_integer(num)
+    assert type_compatible.is_integer(snum)
+
+def test_compatible_posgresql_double():
+    real = "real"
+    assert type_compatible.is_double(real)
+
+def test_compatible_posgresql_interval():
+    interval = "interval"
+    assert type_compatible.is_interval(interval)
+
+def test_compatible_posgresql_boolean():
+    boolean = "boolean"
+    assert type_compatible.is_boolean(boolean)
+
+def test_compatible_posgresql_date():
+    date = "date"
+    timestamp = "timestamp without time zone"
+    timestampZ = "timestamp with time zone"
+
+    assert type_compatible.is_date(date)
+    assert type_compatible.is_date(timestamp)
+    assert type_compatible.is_date(timestampZ)
--- type_compatible.py
+++ type_compatible.py
@@ -1,9 +1,31 @@
-
 ACCEPTED_STRING_DB_TYPES = (
     'character',
     'character varying',
     'national character',
 )
-
+ACCEPTED_INTEGER_DB_TYPES = ('integer','smallint')
+ACCEPTED_DOUBLE_DB_TYPES = ('real',)
+ACCEPTED_INTERVAL_DB_TYPES = ('interval')
+ACCEPTED_BOOLEAN_DB_TYPES = ('boolean')
+ACCEPTED_DATE_DB_TYPES = (
+    'date',
+    'timestamp with time zone',
+    'timestamp without time zone'
+)
 def is_string(db_type):
     return (db_type in ACCEPTED_STRING_DB_TYPES)
+
+def is_integer(db_type):
+    return (db_type in ACCEPTED_INTEGER_DB_TYPES)
+
+def is_double(db_type):
+    return (db_type in ACCEPTED_DOUBLE_DB_TYPES)
+
+def is_interval(db_type):
+    return (db_type in ACCEPTED_INTERVAL_DB_TYPES)
+
+def is_boolean(db_type):
+    return (db_type in ACCEPTED_BOOLEAN_DB_TYPES)
+
+def is_date(db_type):
+    return (db_type in ACCEPTED_DATE_DB_TYPES)
