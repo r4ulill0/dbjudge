135d430d70b90092bd345aab1a11fe6530a0c1b1
raumedi <raul.medina@alumnos.uva.es>
2019-05-01 18:06:21 +0200

    added new accepted date type and readability minor fix

--- type_compatible.py
+++ type_compatible.py
@@ -5,16 +5,17 @@ ACCEPTED_STRING_DB_TYPES = (
 )
 ACCEPTED_INTEGER_DB_TYPES = ('integer','smallint')
 ACCEPTED_FLOAT_DB_TYPES = ('real',)
-ACCEPTED_INTERVAL_DB_TYPES = ('interval')
 ACCEPTED_BOOLEAN_DB_TYPES = ('boolean')
+ACCEPTED_INTERVAL_DB_TYPES = ('interval')
 ACCEPTED_DATE_DB_TYPES = (
     'date',
+    'time without time zone',
     'timestamp with time zone',
     'timestamp without time zone'
 )
 BYTES_OF = {
-    "integer": 4,
-    "smallint": 2,
+    'integer': 4,
+    'smallint': 2,
 }
 def is_string(db_type):
     return (db_type in ACCEPTED_STRING_DB_TYPES)
