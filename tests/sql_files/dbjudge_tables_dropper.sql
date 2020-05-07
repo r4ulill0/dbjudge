2072bffae48fe427bcce4050c1e355b68db1fcf6
raumedi <raul.medina@alumnos.uva.es>
2020-05-07 18:58:06 +0200

    FIXED Major test bug Without this, some test fails during testing may cause database errors due to duplicated tables, or key constraints

--- tests/sql_files/dbjudge_tables_dropper.sql
+++ tests/sql_files/dbjudge_tables_dropper.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS dbjudge_keyword_selection;
 DROP TABLE IF EXISTS dbjudge_question;
 DROP TABLE IF EXISTS dbjudge_database;
 DROP TABLE IF EXISTS dbjudge_fake_data;
+DROP TABLE IF EXISTS dbjudge_keyword;
