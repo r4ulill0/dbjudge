d144612446a60dded8ae5b1aaf1646e30f149240
raumedi <raul.medina@alumnos.uva.es>
2020-05-21 18:25:13 +0200

    Added documentation

--- dbjudge/questions/generation/constraints_truth_table.py
+++ dbjudge/questions/generation/constraints_truth_table.py
@@ -1,5 +1,9 @@
+"""Tools to manage constraints permutations of complex queries."""
+
 
 class Truth_table():
+    """Truth table object to keep track of permutations of boolean states.
+    """
 
     def __init__(self, query_list):
         self.state_size = len(query_list)
@@ -7,8 +11,19 @@ class Truth_table():
         self.table = set()
 
     def add(self, check_row):
+        """Adds a permutation to the table.
+        Each permutation is unique to each table
+        and all permutations in a truth table must have the same size.
+
+        :param check_row: Multiple ordered boolean values
+        """
         if check_row and len(check_row) == self.state_size:
             self.table.add(check_row)
 
     def is_completed(self):
+        """Checks if the truth table has all its possible permutations.
+
+        :return: True if all possible permutations are stored, False otherwise.
+        :rtype: boolean
+        """
         return len(self.table) == self.table_size
