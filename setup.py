a2624a7e16e91a5fb2e38f34a17859f55a3a90c5
raumedi <raul.medina@alumnos.uva.es>
2020-05-27 16:32:59 +0200

    updated version and dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dbjudge',
-    version='0.5.0a',
+    version='1.0.0a',
     description='A SQL judge for postgresql',
     author='Raúl Medina',
     author_email='raulmgcontact@gmail.com',
@@ -10,5 +10,6 @@ setup(
     install_requires=[
         'psycopg2',
         'xeger',
+        'sqlparse',
     ],
 )
