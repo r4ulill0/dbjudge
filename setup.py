6954a5ff92c2acef795126ea10d6e88b4a2d8a2e
raumedi <raul.medina@alumnos.uva.es>
2020-07-05 19:31:44 +0200

    new pre-release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dbjudge',
-    version='1.0.0a2',
+    version='1.0.0a3',
     description='A SQL judge for postgresql',
     author='Raúl Medina González',
     author_email='raulmgcontact@gmail.com',
